182a28426227cfc044f806f8f66d6d18aacc6be7
VVP2024 <sjavka23@ya.lv>
2024-05-29 20:09:18 +0300

    Nodarbiba_Nr_3

--- Nodarbiba_Nr_3/JS/second_script_002.js
+++ Nodarbiba_Nr_3/JS/second_script_002.js
@@ -1,2 +1,34 @@
 a = 10;
 console.log("a mainigais:" +a);
+
+var x = 10;
+    console.log("x mainigais pirms:" +x)
+    
+    {
+    x = 200;
+    console.log("x mainigais ieks un pec:" +x)
+    var x = 100;
+    console.log("x mainigais ieks un pec var:" +x)
+    }
+    console.log("x mainigais pec:" +x)
+    x = 0;
+    console.log("x mainigais pec:" +x)
+    var x = 100000;
+    console.log("x mainigais ieks un pec var:" +x)
+
+    let y = 10;
+    console.log("y mainigais pirms:" +y)
+
+    { 
+       //console.log("y mainigais pirms:" +y)
+    //y = 200;
+    //console.log("y mainigais ieks un pec:" +y)
+    let y = 100;
+    console.log("y mainigais ieks un pec var:" +y)
+    }
+    console.log("y mainigais pec:" +y)
+    y = 0;
+    console.log("y mainigais pec:" +y)
+    // let y = 10000; - nedrikst
+
+    console.log("y mainigais ieks un pec var:" +y)
