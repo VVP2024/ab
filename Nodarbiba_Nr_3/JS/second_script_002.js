2154a524c63c5d79b50d8fb6954276ebe9a0c997
VVP2024 <sjavka23@ya.lv>
2024-05-29 20:38:17 +0300

    Nodarbiba_Nr_3

--- Nodarbiba_Nr_3/JS/second_script_002.js
+++ Nodarbiba_Nr_3/JS/second_script_002.js
@@ -1,6 +1,7 @@
 a = 10;
 console.log("a mainigais:" +a);
 
+// tests var//
 var x = 10;
     console.log("x mainigais pirms:" +x)
     
@@ -15,7 +16,7 @@ var x = 10;
     console.log("x mainigais pec:" +x)
     var x = 100000;
     console.log("x mainigais ieks un pec var:" +x)
-
+//tests let//
     let y = 10;
     console.log("y mainigais pirms:" +y)
 
@@ -32,3 +33,29 @@ var x = 10;
     // let y = 10000; - nedrikst
 
     console.log("y mainigais ieks un pec var:" +y)
+
+    //tests const//
+
+    const k = 100;
+    console.log("k mainigais pirms:" +k)
+    
+    {
+    //k = 2000;
+    //console.log("k mainigais ieks un pec:" +k)
+    const k = 1000;
+    console.log("k mainigais ieks un pec var:" +k)
+    }
+    console.log("k mainigais pec:" +k)
+    //k = 0;
+    console.log("k mainigais pec:" +k)
+    //const x = 1000000000;
+    console.log("k mainigais ieks un pec var:" +k)
+    //let x = 100;
+    //let y = 25;
+    //let k = 666;
+    var x = 111;
+    //var y = 222;
+    //var k = 333;
+//const x = 777;
+//const y = 888;
+//const k = 999;
