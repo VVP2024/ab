aea0210d0d59807880f6f2e08f2d10bcb6ad08ef
VVP2024 <sjavka23@ya.lv>
2024-06-06 00:39:03 +0300

    test

---
+++ tests/js/tests.js
@@ -0,0 +1,37 @@
+
+let now = new Date();
+let hour_x = 20;
+let moorning = 10;
+let day = 18;
+let night_a = 22;
+let night_b = 4;
+let time_shift = 0;
+
+//now = now + time_shift;
+now.setHours(now.getHours() + time_shift);
+
+console.log(now);
+console.log(typeof(now));
+
+console.log(now.getHours());
+console.log(typeof(now.getHours()));
+
+console.log(now.getHours() < hour_x);
+console.log(typeof(now.getHours() < hour_x));
+  
+// if (nosacijums){darbibas} else{darbibas};
+//if (nosacijums){darbibas} else if (nosacijums) {darbibas};
+if (now.getHours() > night_b && now.getHours() < moorning) {
+  document.getElementById("demo").innerHTML = "Good moorning!";
+}
+
+else if (now.getHours() > moorning && now.getHours() < day) {
+    document.getElementById("demo").innerHTML = "Good day!";
+  }
+else if (now.getHours() > day && now.getHours() < night_a) {
+    document.getElementById("demo").innerHTML = "Good evening!";
+  }
+
+else if (now.getHours() > night_a || now.getHours() < night_b) {
+    document.getElementById("demo").innerHTML = "Good night!";
+  }
