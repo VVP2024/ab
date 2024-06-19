2059df0ed38ba8fafdd3db96f02c2b943fdfecf6
VVP2024 <sjavka23@ya.lv>
2024-06-19 20:17:12 +0300

    nod_09

--- Nodarbiba_Nr_9/JS/first.js
+++ Nodarbiba_Nr_9/JS/first.js
@@ -23,13 +23,17 @@ window.focus('http://127.0.0.1:9222/Nodarbiba_Nr_8/index.html');
 Window.close ('http://127.0.0.1:9222/Nodarbiba_Nr_8/index.html');
 */
 
-let jsWindow = window.open(
+/*let jsWindow = window.open(
     //"https://rtu.lv",
     "http://127.0.0.1:9222/Nodarbiba_Nr_8/index.html",
     //"RTU",
     "NR08",
     'height=600,width=800');
 
+
+    setTimeout(() => {
+        alert('10 seconds has been passed!')
+    }, 10000);
 setTimeout(() => {
     jsWindow.resizeTo(600, 300);
 }, 3000);
@@ -41,3 +45,13 @@ setTimeout(() => {
 setTimeout(() => {
     jsWindow.moveTo(500, 500);
 }, 5000);
+*/
+let result_c = confirm('Vai dzēst izvēlēto saturu?');
+let message_c = result_c ? 'Jūs teicāt - jā' : 'Jūs teicāt - nē';
+alert(message_c);
+
+let result_p = prompt('Ievadiet šī gada numuru:','2024');
+let message_p = result_p ? 'Šobrīd ir '+result_p+'. gads' : '???. gads';
+alert(message_p);
+
+console.log(result_p * result_p);
